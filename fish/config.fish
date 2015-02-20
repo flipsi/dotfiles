f9bdd5f16d8915e2a53c42b580c7860b5b91d91e
sflip <soziflip@gmail.com>
2015-02-20 04:56:18 +0100

    mostly sublime updates

--- fish/config.fish
+++ fish/config.fish
@@ -6,8 +6,9 @@ set fish_greeting ""
 set PATH /opt/ghc-7.8.2/bin $PATH
 set PATH /opt/ghc-7.6.3/bin $PATH
 set PATH /opt/haskell-platform-2013.2.0.0/bin/ $PATH
-set PATH /home/flip/.cabal/bin $PATH
 set PATH /opt/cc0/bin $PATH
+set PATH ~/.cabal/bin $PATH
+set PATH ~/bin $PATH
 
 set -x LESSOPEN "| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
 set -x LESS " -c -R "
