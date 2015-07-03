ad9b94096ac987dfeca8d97e7c2cb78784fb9eaf
Philipp Moers <soziflip@gmail.com>
2015-07-04 01:19:23 +0200

    alt+i for file browser again

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,9 +1,7 @@
 function fish_user_key_bindings
-	bind \eo ranger
-	bind \ee elinks
+	bind \ei ranger
 	bind \e. history-token-search-backward
 	bind \e: history-token-search-forward
 	bind \eh backward-char
 	bind \el forward-char
-	bind \ek backward-kill-word
 end
