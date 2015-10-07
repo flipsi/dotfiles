7cb9b5fd500c14d1ae012b8691c150e3f06ce955
Philipp Moers <soziflip@gmail.com>
2015-10-07 21:37:41 +0200

    added xfce4/xfconf directory. all kinds of xfce stuff

--- install.sh
+++ install.sh
@@ -142,6 +142,7 @@ function create_link_for_target() {
         xfce4)
             mkdir -p $HOME/.config/xfce4
             mkdir -p $HOME/.config/xfce4/terminal
+            create_link $PWD/xfce4/xfconf $HOME/.config/xfce4/xfconf
             create_link $PWD/xfce4/terminal/terminalrc $HOME/.config/xfce4/terminal/terminalrc
             create_link $PWD/xfce4/xfce4-screenshooter $HOME/.config/xfce4/xfce4-screenshooter
             ;;
