3b136795255db56da6657d4299127c1ca395eeb1
Philipp Moers <soziflip@gmail.com>
2015-11-03 19:28:31 +0100

    alt+s in fish for git status

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,6 @@
 function fish_user_key_bindings
 	bind \ei ranger
+    bind \es 'git status; fish_prompt'
 	bind \e. history-token-search-backward
 	bind \e: history-token-search-forward
 	bind \eh backward-char
