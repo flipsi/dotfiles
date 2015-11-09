c334a0fb3bd465bcd3e38542b333199fd8255d9e
Philipp Moers <soziflip@gmail.com>
2015-11-09 19:31:27 +0100

    [fish] alt+s git status works in subdirectories

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,6 @@
 function fish_user_key_bindings
 	bind \ei ranger
-    bind \es 'commandline ""; echo; ll; if test -e .git; echo; git status; end; fish_prompt'
+    bind \es 'commandline ""; echo; ll; echo; git status ^/dev/null; fish_prompt'
 	bind \e. history-token-search-backward
 	bind \e: history-token-search-forward
 	bind \eh backward-char
