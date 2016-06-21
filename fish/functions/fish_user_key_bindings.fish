9fc2c9edb61c50e067b9a828faea7fd41e48d18b
Philipp Moers <soziflip@gmail.com>
2016-06-21 02:14:44 +0200

    fish: better ranger handling
    show cwd in prompt in red when not within ranger
    and blue (like in ranger) when within ranger.
    clear screen after exiting ranger.

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,5 @@
 function fish_user_key_bindings
-	bind \ei ranger
+	bind \ei 'ranger; and clear; and fish_prompt'
     bind \es 'commandline ""; echo; ll; echo; git status ^/dev/null; fish_prompt'
 	bind \e. history-token-search-backward
 	bind \e: history-token-search-forward
