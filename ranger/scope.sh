2efcc44918e58ecd7c582a052f593c882b924b7f
Philipp Moers <soziflip@gmail.com>
2016-09-06 13:02:49 +0200

    ranger: use unar (unar/ar/lsar) for archives

--- ranger/scope.sh
+++ ranger/scope.sh
@@ -47,6 +47,7 @@ case "$extension" in
     # Archive extensions:
     7z|a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|\
     rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)
+        try lsar "$path" && { dump | trim; exit 0; }
         try als "$path" && { dump | trim; exit 0; }
         try acat "$path" && { dump | trim; exit 3; }
         try bsdtar -lf "$path" && { dump | trim; exit 0; }
