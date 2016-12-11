30611e5d6b35ce31a51148685aa26df2f572e439
Philipp Moers <soziflip@gmail.com>
2016-12-11 04:33:32 +0100

    fish: better config

--- fish/config.fish
+++ fish/config.fish
@@ -1,37 +1,50 @@
 # don't show welcome message
 set fish_greeting ""
 
-# find out host once (useful for multiple things, but saves cycles)
-set hostname (hostname)
 
 
-# set PATH /opt/ghc-7.8.2/bin $PATH
-# set PATH /opt/ghc-7.6.3/bin $PATH
-# set PATH /opt/haskell-platform-2013.2.0.0/bin/ $PATH
-# set PATH /usr/local/haskell/ghc-7.8.3-x86_64/bin/ $PATH
-# set PATH /opt/cc0/bin $PATH
-# set PATH ~/.cabal/bin $PATH
-set PATH ~/bin $PATH
-set PATH ~/shellscripts $PATH
+# only do this stuff when login shell...
+if status --is-login
+
+    # QUICKFIX FOR LOCALE ISSUE
+    set -x LANG de_DE.UTF-8
+
+    # find out host once (useful for multiple things, but saves cycles)
+    set hostname (hostname)
+
+    # some more stuff in PATH
+    set -x PATH ~/bin $PATH
+    set -x PATH ~/shellscripts $PATH
+
+    # edit files with vim
+    set -x EDITOR vim
+
+    # music player daemon
+    # if test $hostname = "asterix"
+    #     set -x MPD_HOST (ifconfig wlan0 | grep 'inet ' | sed 's/.*addr:\(.*\)  Bcast.*/\1/')
+    # end
+    set -x MPD_HOST $hostname
 
 
-# edit files with vim
-set -x EDITOR vim
-
-# music player daemon
-# if test $hostname = "asterix"
-#     set -x MPD_HOST (ifconfig wlan0 | grep 'inet ' | sed 's/.*addr:\(.*\)  Bcast.*/\1/')
-# end
-set -x MPD_HOST $hostname
+    # keychain takes care of ssh-agent and can also do gpg
+    if test $hostname = "asterix" -o $hostname = "obelix"
+        eval (command keychain --eval --quiet ~/.ssh/id_rsa)
+    end
 
 
-# keychain takes care of ssh-agent and can also do gpg
-if test $hostname = "asterix" -o $hostname = "obelix"
-    eval (command keychain --eval --quiet ~/.ssh/id_rsa)
+    # correct typos the fun way
+    # https://github.com/nvbn/thefuck
+    # (the following does not work, so i added the function manually)
+    #eval (thefuck --alias)
+
+
+    # on some hosts, start desktop if not already running
+    if test $hostname = "obelix"
+        if not set -q DISPLAY
+            if not pgrep -x xinit >/dev/null
+                startx
+            end
+        end
+    end
+
 end
-
-
-# correct typos the fun way
-# https://github.com/nvbn/thefuck
-# (the following does not work, so i added the function manually)
-#eval (thefuck --alias)
