5e1f4b323671ee88335a5a654b9957a0e904cd28
Philipp Moers <soziflip@gmail.com>
2017-05-08 22:24:24 +0200

    mutt: also install elinks for mail preview

--- install.sh
+++ install.sh
@@ -404,7 +404,7 @@ function package_install_target() {
             ;;
 
         mutt )
-            LIST_OF_SYSTEM_PACKAGES="mutt msmtp offlineimap notmuch notmuch-mutt goobook"
+            LIST_OF_SYSTEM_PACKAGES="mutt msmtp offlineimap notmuch notmuch-mutt goobook elinks"
             ;;
 
         taskwarrior )
