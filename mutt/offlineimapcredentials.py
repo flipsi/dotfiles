9b0f58e8e957133a9a45643fefb19dfeac37c77d
Philipp Moers <soziflip@gmail.com>
2017-05-08 22:27:17 +0200

    mutt: add onpage.org (and passwords with pass)

--- mutt/offlineimapcredentials.py
+++ mutt/offlineimapcredentials.py
@@ -1,19 +1,25 @@
 #! /usr/bin/env python2
 
-
 # Get passwords from gpg encrypted files
-
-# Prepare files with passwords for each account:
-#   vim ~/.mutt/credentials/ACCOUNT
-# Encrypt them with GPG:
-#   gpg -e -r 'Philipp Moers <soziflip@gmail.com>' ~/.mutt/credentials/ACCOUNT
-# And get rid of the cleartext password files:
-#   shred ~/.mutt/credentials/ACCOUNT; rm -f ~/.mutt/credentials/ACCOUNT
 
 
 
 from os import environ, path
 from subprocess import check_output
+
+
+
+###########################
+# OPTION 1: OWN GPG FILES #
+###########################
+
+## Prepare files with passwords for each account:
+##   vim ~/.mutt/credentials/ACCOUNT
+## Encrypt them with GPG:
+##   gpg -e -r 'Philipp Moers <soziflip@gmail.com>' ~/.mutt/credentials/ACCOUNT
+## And get rid of the cleartext password files:
+##   shred ~/.mutt/credentials/ACCOUNT; rm -f ~/.mutt/credentials/ACCOUNT
+
 
 def get_password(account):
   account = path.basename(account)
@@ -24,3 +30,25 @@ def get_password(account):
     return check_output(args).strip()
   except CalledProcessError:
     return ""
+
+
+
+##############################
+# OPTION 2: PASSWORD STORAGE #
+##############################
+
+#  def get_password(account):
+  #  account = path.basename(account)
+  #  passPath = {
+    #  'onpage.org': 'google/philipp.moers@onpage.org',
+    #  'mail.de': 'mail.de/philipp.moers@mail.de',
+    #  'gmail.com': 'google/soziflip',
+    #  'web.de-pmoers': 'web.de/pmoers@web.de',
+    #  'web.de-keinschweinehund': 'web.de/keinschweinehund@web.de',
+  #  }.get(account)
+  #  #  args = ["gpg", "--use-agent", "--quiet", "--batch", "-d", credentialfile]
+  #  args = ["pass", passPath]
+  #  try:
+    #  return check_output(args).strip()
+  #  except CalledProcessError:
+    #  return ""
