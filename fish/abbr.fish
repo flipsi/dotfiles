b23f7e6478b3de3a4c6ae15bb59293ead3226025
Philipp Moers <soziflip@gmail.com>
2017-06-18 00:49:55 +0200

    fish: git abbreviations

--- fish/abbr.fish
+++ fish/abbr.fish
@@ -8,13 +8,19 @@ abbr gb     git branch --verbose
 abbr gc     git commit
 abbr gca    git commit --amend
 abbr gd     git diff
+abbr gda    git diff --cached
 abbr gdc    git diff --cached
+abbr ge     git checkout
 abbr gl     tig log
 abbr glh    git --no-pager log -n 1
 abbr go     git clone --recursive
 abbr gp     git pull
+abbr gpu    git push
+abbr gup    git push --set-upstream
 abbr gr     grep --with-filename --line-number --recursive --ignore-case --extended-regexp
-abbr gs     git stash --include-untracked
+abbr gs     git status
+abbr gsa    git stash save
+abbr gsi    git stash save --include-untracked
 abbr gsp    git stash pop
 abbr k      setxkbmap de_sflip
 abbr h      htop
