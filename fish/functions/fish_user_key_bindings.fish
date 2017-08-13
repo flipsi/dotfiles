6f72080942ebfc07c349eb80588556c2b1599a57
Philipp Moers <soziflip@gmail.com>
2017-08-13 13:21:43 +0200

    fish: copy last command to clipboard with alt+c

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,7 @@
 function fish_user_key_bindings
     bind \ei 'ranger; and clear; and fish_prompt'
     bind \es 'commandline ""; echo; ll; echo; git status --short ^/dev/null; fish_prompt'
+    bind \ec 'history | head -n 1 | tr -d "\n" | xsel -b'
     bind \e. history-token-search-backward
     bind \e: history-token-search-forward
     bind \eh backward-char
