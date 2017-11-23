e0a108de8a7aed10265723d527d34460a0d75043
Philipp Moers <soziflip@gmail.com>
2017-11-23 01:33:49 +0100

    fish: fix 'man' function async problem

--- fish/functions/man.fish
+++ fish/functions/man.fish
@@ -6,5 +6,5 @@ function man --description 'man pages with colors'
     set -x LESS_TERMCAP_so (printf "\e[01;44;33m")   # begin standout-mode - info box
     set -x LESS_TERMCAP_ue (printf "\e[0m")          # end underline
     set -x LESS_TERMCAP_us (printf "\e[01;32m")      # begin underline
-    eval (which man) $argv
+    bash -c "man $argv"
 end
