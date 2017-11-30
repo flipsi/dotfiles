210443043bdfecf3ec37c37a4202f691cc548c06
Philipp Moers <soziflip@gmail.com>
2017-11-30 23:43:48 +0100

    fish: watch last command with alt+w

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -2,6 +2,7 @@ function fish_user_key_bindings
     bind \ei 'ranger; and clear; and fish_prompt'
     bind \es 'commandline ""; echo; ll; echo; git status --short ^/dev/null; fish_prompt'
     bind \ec 'history | head -n 1 | tr -d "\n" | xsel -b'
+    bind \ew 'watch -c (history | head -n 1)'
     bind \e. history-token-search-backward
     bind \e: history-token-search-forward
     bind \eh backward-char
