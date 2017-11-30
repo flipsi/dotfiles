03e54fc7e09835303d44e87716ab0ce0ae5447a6
Philipp Moers <soziflip@gmail.com>
2017-11-30 23:45:05 +0100

    fish: ctrl+r to search command history with fzf

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -3,6 +3,7 @@ function fish_user_key_bindings
     bind \es 'commandline ""; echo; ll; echo; git status --short ^/dev/null; fish_prompt'
     bind \ec 'history | head -n 1 | tr -d "\n" | xsel -b'
     bind \ew 'watch -c (history | head -n 1)'
+    bind \cr '__fzf_reverse_isearch'
     bind \e. history-token-search-backward
     bind \e: history-token-search-forward
     bind \eh backward-char
