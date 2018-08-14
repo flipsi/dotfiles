91e3baa17454cfff42aea541aa67c1b8456b7358
Philipp Moers <soziflip@gmail.com>
2018-08-14 20:27:35 +0200

    [minor] fish: reorder key bindings

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,12 +1,12 @@
 function fish_user_key_bindings
-    bind \ei 'ranger; and clear; and fish_prompt'
-    bind \es 'commandline ""; echo; ll; echo; git status --short ^/dev/null; fish_prompt'
-    bind \ec 'history | head -n 1 | tr -d "\n" | xsel -b'
-    bind \ew 'watch -c (history | head -n 1)'
     bind \cr '__fzf_reverse_isearch'
     bind \e. history-token-search-backward
     bind \e: history-token-search-forward
+    bind \ec 'history | head -n 1 | tr -d "\n" | xsel -b'
     bind \eh backward-char
+    bind \ei 'ranger; and clear; and fish_prompt'
     bind \el forward-char
+    bind \es 'commandline ""; echo; ll; echo; git status --short ^/dev/null; fish_prompt'
     bind \et 'commandline -i (echo -n (date +"%F-%H%M"))'
+    bind \ew 'watch -c (history | head -n 1)'
 end
