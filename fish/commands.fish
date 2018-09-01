4449977ea58f15d29dc5927716b866377deb1d74
Philipp Moers <soziflip@gmail.com>
2018-09-01 17:35:48 +0200

    fish: add weather command

--- fish/commands.fish
+++ fish/commands.fish
@@ -1,3 +1,4 @@
 # this is a collection of useful commands that might get lost if not written down
-     
+
 git checkout --quiet --detach; git fetch (git remote | head -n1) master:master; git checkout --quiet -
+curl wttr.in # weather report
