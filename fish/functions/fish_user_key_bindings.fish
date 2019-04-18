b2c1cb9f8c37f6445f50d8422aa36d0218f2be0c
Philipp Moers <soziflip@gmail.com>
2019-04-18 23:00:40 +0200

    jump with alt+l

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -4,10 +4,8 @@ function fish_user_key_bindings
     bind \e. history-token-search-backward
     bind \e: history-token-search-forward
     bind \ec 'history | head -n 1 | tr -d "\n" | xsel -b'
-    bind \eh backward-char
     bind \ei 'ranger; and clear; and fish_prompt'
-    bind \ej 'cd_interactively; and clear; and fish_prompt'
-    bind \el forward-char
+    bind \el 'cd_interactively; and clear; and fish_prompt'
     bind \es 'commandline ""; ll; echo; git status --short ^/dev/null; fish_prompt'
     bind \et 'commandline -i (echo -n (date +"%F-%H%M"))'
     bind \eu 'commandline ""; cd ..; clear; fish_prompt'
