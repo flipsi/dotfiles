d9833515cc6adfbd3f8c85512b68b647f54a0072
Philipp Moers <soziflip@gmail.com>
2019-07-19 14:53:29 +0200

    i3: add initialize outputs --only support on dwarf

--- i3/bin/i3-initialize-outputs.sh
+++ i3/bin/i3-initialize-outputs.sh
@@ -50,7 +50,13 @@ case $(hostname) in
 
     dwarf )
 
-        if xrandr | grep 'DP3-1 connected' >/dev/null; then
+        if test -n "$ONLY"; then
+
+            xrandr --output DP3-1 --off
+            xrandr --output DP3-2 --off
+            xrandr --output eDP1 --mode '1920x1080' --auto
+
+        elif xrandr | grep 'DP3-1 connected' >/dev/null; then
 
             xrandr --output VIRTUAL1 --off --output DP3 --off --output eDP1 --primary --mode 1920x1080 --pos 3840x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output DP3-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP3-3 --off --output DP3-2 --mode 1920x1080 --pos 1920x0 --rotate normal \
                 || ( \
