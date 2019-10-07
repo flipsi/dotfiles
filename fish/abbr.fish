8e290251556ccf2ab63b9c9ba59b2e267eada4dc
Philipp Moers <soziflip@gmail.com>
2019-10-07 23:37:25 +0200

    fish: update abbr for startup script

--- fish/abbr.fish
+++ fish/abbr.fish
@@ -1,100 +1,100 @@
-abbr ca     gcalcli
-abbr cdi    cd_interactively
-abbr cdi.   cd_interactively .
-abbr cdir   cd_interactively /
-abbr d      docker
-abbr da     docker-compose up -d
-abbr do     docker-compose stop
-abbr dce    docker-compose exec
-abbr dcl    docker-compose logs
-abbr dclf   docker-compose logs -f
-abbr dcp    docker-compose pull
-abbr fi     fzf_wrapper --find --ranger
-abbr fl     fzf_wrapper --locate --ranger
-abbr g      git
-abbr ga     git add
-abbr gaa    git add --all
-abbr gas    git save
-# abbr gap    git pop  # because of autocompletion, the following is better:
-abbr gap    git rebase \(git rev-parse --abbrev-ref HEAD\) stash/
-abbr gb     git branch
-abbr gB     git branch --verbose --all
-abbr gbd    git branch -d
-abbr gbD    git branch -D
-abbr gbm    git branch -m
-abbr gc     git commit
-abbr gca    git commit --amend
-abbr gcl    git clone --recursive
-abbr gd     git diff
-abbr gda    git diff --cached
-abbr gdc    git diff --cached
-abbr ge     git checkout
-abbr geb    git checkout -b
-abbr gf     git fetch --prune
-abbr gfm    git checkout --quiet --detach\; git fetch \(git remote \| head -n1\) master:master\; git checkout --quiet -
-abbr gl     tig
-abbr glh    git --no-pager log -n 1
-abbr gli    git --no-pager log -n 1 \| head -n 1 \| cut -d\" \" -f2 \| xsel --clipboard
-abbr gm     git merge
-abbr gmm    git merge master
-abbr gp     git pull
-abbr gpp    git pull
-abbr gpu    git push
-abbr gpuf   git push --force-with-lease
-abbr gpuu   git push -u \(git remote \| head -n1\) \(git rev-parse --abbrev-ref HEAD\)
-abbr gr     grep --with-filename --line-number --recursive --ignore-case --extended-regexp
-abbr gra    git reset --abort
-abbr grc    git reset --continue
-abbr grh    git reset --hard
-abbr grH    git reset HEAD~1
-abbr grm    git rebase master
-abbr gs     tig status
-abbr gsa    git stash push
-abbr gsi    git stash push --include-untracked
-abbr gsp    git stash pop
-abbr gst    git status
-abbr gu     git submodule
-abbr gup    git submodule update
-abbr gus    git submodule status
-abbr h      htop
-abbr l      less
-abbr ll     ls -lhF
-abbr lll    ls -lhFa
-abbr m      man
-abbr ma     make apply
-abbr mi     make init
-abbr mip    make init plan
-abbr mkd    mkdir
-abbr mp     make plan
-abbr mpa    make plan apply
-abbr o      open
-abbr p      pass
-abbr pe     pass edit
-abbr pg     pgrep
-abbr pgp    pass git pull
-abbr pgpu   pass git push
-abbr pk     pkill
-abbr rrm    rm -rf
-abbr se     sudoedit
-abbr r      ranger
-abbr s      systemctl status
-abbr sa     systemctl start
-abbr so     systemctl stop
-abbr sr     systemctl restart
-abbr t      trans :de
-abbr te     trans de:en
-abbr tf     trans de:fr
-abbr u      unison
-abbr v      vim
-abbr vs     vim --servername \(basename \(pwd\)\)
-abbr x      xrandr
-abbr xo     xrandr --output
-abbr y      yaourt
-abbr yi     yaourt -Qi
-abbr yr     yaourt -R
-abbr ys     yaourt -S
-abbr ysi    yaourt -Si
-abbr yu     yaourt -Syu
-abbr yun    yaourt -Syu --noconfirm
-abbr yua    yaourt -Syua
-abbr yuan   yaourt -Syua --noconfirm
+abbr --add --global ca     gcalcli
+abbr --add --global cdi    cd_interactively
+abbr --add --global cdi.   cd_interactively .
+abbr --add --global cdir   cd_interactively /
+abbr --add --global d      docker
+abbr --add --global da     docker-compose up -d
+abbr --add --global do     docker-compose stop
+abbr --add --global dce    docker-compose exec
+abbr --add --global dcl    docker-compose logs
+abbr --add --global dclf   docker-compose logs -f
+abbr --add --global dcp    docker-compose pull
+abbr --add --global fi     fzf_wrapper --find --ranger
+abbr --add --global fl     fzf_wrapper --locate --ranger
+abbr --add --global g      git
+abbr --add --global ga     git add
+abbr --add --global gaa    git add --all
+abbr --add --global gas    git save
+# abbr --add --global gap    git pop  # because of autocompletion, the following is better:
+abbr --add --global gap    git rebase \(git rev-parse --abbr --add --globalev-ref HEAD\) stash/
+abbr --add --global gb     git branch
+abbr --add --global gB     git branch --verbose --all
+abbr --add --global gbd    git branch -d
+abbr --add --global gbD    git branch -D
+abbr --add --global gbm    git branch -m
+abbr --add --global gc     git commit
+abbr --add --global gca    git commit --amend
+abbr --add --global gcl    git clone --recursive
+abbr --add --global gd     git diff
+abbr --add --global gda    git diff --cached
+abbr --add --global gdc    git diff --cached
+abbr --add --global ge     git checkout
+abbr --add --global geb    git checkout -b
+abbr --add --global gf     git fetch --prune
+abbr --add --global gfm    git checkout --quiet --detach\; git fetch \(git remote \| head -n1\) master:master\; git checkout --quiet -
+abbr --add --global gl     tig
+abbr --add --global glh    git --no-pager log -n 1
+abbr --add --global gli    git --no-pager log -n 1 \| head -n 1 \| cut -d\" \" -f2 \| xsel --clipboard
+abbr --add --global gm     git merge
+abbr --add --global gmm    git merge master
+abbr --add --global gp     git pull
+abbr --add --global gpp    git pull
+abbr --add --global gpu    git push
+abbr --add --global gpuf   git push --force-with-lease
+abbr --add --global gpuu   git push -u \(git remote \| head -n1\) \(git rev-parse --abbr --add --globalev-ref HEAD\)
+abbr --add --global gr     grep --with-filename --line-number --recursive --ignore-case --extended-regexp
+abbr --add --global gra    git reset --abort
+abbr --add --global grc    git reset --continue
+abbr --add --global grh    git reset --hard
+abbr --add --global grH    git reset HEAD~1
+abbr --add --global grm    git rebase master
+abbr --add --global gs     tig status
+abbr --add --global gsa    git stash push
+abbr --add --global gsi    git stash push --include-untracked
+abbr --add --global gsp    git stash pop
+abbr --add --global gst    git status
+abbr --add --global gu     git submodule
+abbr --add --global gup    git submodule update
+abbr --add --global gus    git submodule status
+abbr --add --global h      htop
+abbr --add --global l      less
+abbr --add --global ll     ls -lhF
+abbr --add --global lll    ls -lhFa
+abbr --add --global m      man
+abbr --add --global ma     make apply
+abbr --add --global mi     make init
+abbr --add --global mip    make init plan
+abbr --add --global mkd    mkdir
+abbr --add --global mp     make plan
+abbr --add --global mpa    make plan apply
+abbr --add --global o      open
+abbr --add --global p      pass
+abbr --add --global pe     pass edit
+abbr --add --global pg     pgrep
+abbr --add --global pgp    pass git pull
+abbr --add --global pgpu   pass git push
+abbr --add --global pk     pkill
+abbr --add --global rrm    rm -rf
+abbr --add --global se     sudoedit
+abbr --add --global r      ranger
+abbr --add --global s      systemctl status
+abbr --add --global sa     systemctl start
+abbr --add --global so     systemctl stop
+abbr --add --global sr     systemctl restart
+abbr --add --global t      trans :de
+abbr --add --global te     trans de:en
+abbr --add --global tf     trans de:fr
+abbr --add --global u      unison
+abbr --add --global v      vim
+abbr --add --global vs     vim --servername \(basename \(pwd\)\)
+abbr --add --global x      xrandr
+abbr --add --global xo     xrandr --output
+abbr --add --global y      yaourt
+abbr --add --global yi     yaourt -Qi
+abbr --add --global yr     yaourt -R
+abbr --add --global ys     yaourt -S
+abbr --add --global ysi    yaourt -Si
+abbr --add --global yu     yaourt -Syu
+abbr --add --global yun    yaourt -Syu --noconfirm
+abbr --add --global yua    yaourt -Syua
+abbr --add --global yuan   yaourt -Syua --noconfirm
