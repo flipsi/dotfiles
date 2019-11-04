5e16592e7e504e4e0318b9b08345993d44dc625b
Philipp Moers <soziflip@gmail.com>
2019-11-04 20:22:39 +0100

    i3: new outputs on dwarf

--- i3/bin/i3-initialize-outputs.sh
+++ i3/bin/i3-initialize-outputs.sh
@@ -58,7 +58,7 @@ case $(hostname) in
 
         elif xrandr | grep 'DP3-1 connected' >/dev/null; then
 
-            xrandr --output VIRTUAL1 --off --output DP3 --off --output eDP1 --primary --mode 1920x1080 --pos 3840x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output DP3-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP3-3 --off --output DP3-2 --mode 1920x1080 --pos 1920x0 --rotate normal \
+            xrandr --output eDP1 --primary --mode 1920x1080 --pos 3840x0 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output DP3-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP3-2 --mode 1920x1080 --pos 0x0 --rotate normal --output DP3-3 --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VIRTUAL1 --off \
                 || ( \
                 sleep 0.1 && xrandr --output DP3-2 --off && \
                 sleep 0.1 && xrandr --output DP3-1 --auto && \
