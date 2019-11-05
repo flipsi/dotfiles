320c98bd86a0b6aed57d876e5c05991fb3116458
Philipp Moers <soziflip@gmail.com>
2019-11-05 17:10:17 +0100

    Update weather with new password store key

--- fish/functions/weather.fish
+++ fish/functions/weather.fish
@@ -1,3 +1,3 @@
 function weather
-	wego -forecast-api-key (pass darksky.net/soziflip+darksky@gmail.com | grep api-key | cut -d' ' -f2) -l "48.1442327,11.5748727"
+	wego -forecast-api-key (pass darksky.net/gmail | grep api-key | cut -d' ' -f2) -l "48.1442327,11.5748727"
 end
