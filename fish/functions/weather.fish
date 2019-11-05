6df0cb3cfa6869fd1bfca1a2eedd36790bb3d8ff
Philipp Moers <soziflip@gmail.com>
2019-11-05 19:29:49 +0100

    weather with parameter or default to munich

--- fish/functions/weather.fish
+++ fish/functions/weather.fish
@@ -1,3 +1,12 @@
-function weather
-	curl wttr.in
+function weather --description 'show weather forecast'
+
+	set	default_location 'munich'
+
+	set location $argv[1]
+	if test -z $location
+		set location $default_location
+	end
+
+	curl wttr.in/$location
+
 end
