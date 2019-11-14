b46319985612dfffbba9a016ff2de62fc567f69b
Philipp Moers <soziflip@gmail.com>
2019-11-14 20:40:25 +0100

    Improve deleting merged branches

--- fish/commands.fish
+++ fish/commands.fish
@@ -2,7 +2,7 @@
 
 curl wttr.in # weather report
 date +%s # unix timestamp
-git branch --merged | egrep "(feature|hotfix)" | xargs git branch -d # delete merged branches
+git branch --merged -vv | grep "origin/.*: gone]" | cut -d" " -f3 | xargs git branch -D # delete merged branches
 git checkout --quiet --detach; git fetch (git remote | head -n1) master:master; git checkout --quiet -
 sudo pacman -Sc # clear pacman cache (to free disk space)
 yay -Yc # remove unused dependencies
