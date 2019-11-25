33a0adcd9aec84140ea8f943f2925eb8a7bb57c2
Philipp Moers <soziflip@gmail.com>
2019-11-25 14:58:47 +0100

    Don't force deleting branches

--- fish/commands.fish
+++ fish/commands.fish
@@ -2,7 +2,7 @@
 
 curl wttr.in # weather report
 date +%s # unix timestamp
-git branch -vv --merged | grep "\[.*/.*: gone\]" | cut -d" " -f3 | xargs git branch -D # delete merged branches
+git branch -vv --merged | grep "\[.*/.*: gone\]" | cut -d" " -f3 | xargs git branch -d # delete merged branches
 git checkout --quiet --detach; git fetch (git remote | head -n1) master:master; git checkout --quiet -
 sudo pacman -Sc # clear pacman cache (to free disk space)
 yay -Yc # remove unused dependencies
