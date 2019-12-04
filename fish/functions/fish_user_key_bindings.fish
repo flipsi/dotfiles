4813e9e3319e8d30e1f30a3a2b47a179f7266bdd
Philipp Moers <soziflip@gmail.com>
2019-12-04 23:11:27 +0100

    fish: add seconds to generated timestamp

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -7,7 +7,7 @@ function fish_user_key_bindings
     bind \ei 'ranger; and clear; and fish_prompt'
     bind \el 'cd_interactively; and clear; and fish_prompt'
     bind \es 'commandline ""; ll; echo; git status --short ^/dev/null; fish_prompt'
-    bind \et 'commandline -i (echo -n (date +"%F-%H%M"))'
+    bind \et 'commandline -i (echo -n (date +"%F-%H%M%S"))'
     bind \eu 'commandline ""; cd ..; clear; fish_prompt'
     bind \ew 'watch -c (history | head -n 1)'
 end
