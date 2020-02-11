4272e21af8080670ad446dda57099fa386834772
Philipp Moers <soziflip@gmail.com>
2020-02-11 22:16:52 +0100

    fish: clear cache command with yay cache (~/.cache/yay)

--- fish/commands.fish
+++ fish/commands.fish
@@ -4,5 +4,5 @@ curl wttr.in # weather report
 date +%s # unix timestamp
 git branch -vv --merged | grep "\[.*/.*: gone\]" | cut -d" " -f3 | xargs git branch -d # delete merged branches
 git checkout --quiet --detach; git fetch (git remote | head -n1) master:master; git checkout --quiet -
-sudo pacman -Sc # clear pacman cache (to free disk space)
+yay -Sc # clear pacman cache (to free disk space)
 yay -Yc # remove unused dependencies
