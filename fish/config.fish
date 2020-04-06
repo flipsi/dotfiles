a2d1d282ca2200cb4e07a3a0ab70b4f60e7cd145
Philipp Moers <soziflip@gmail.com>
2020-04-06 22:07:40 +0200

    fish: set environment variables from separate file

--- fish/config.fish
+++ fish/config.fish
@@ -6,37 +6,7 @@ set fish_greeting ""
 # environment variables #
 #########################
 
-# QUICKFIX FOR LOCALE ISSUE
-# set -x LANG de_DE.UTF-8
-set -x LANG en_US.UTF-8
-
-# some more stuff in PATH
-set -x PATH ~/.local/bin $PATH
-set -x PATH ~/.cargo/bin $PATH
-set -x PATH ~/.gem/ruby/2.6.0/bin $PATH
-set -x PATH ~/.i3/bin $PATH
-set -x PATH ~/shellscripts $PATH
-set -x PATH ~/bin $PATH
-
-# edit files with vim
-set -x EDITOR vim
-
-# web browser
-set -x BROWSER vivaldi-stable
-
-# setup fzf to use ag instead of find
-set -x FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -f -g ""'
-
-# use rofi instead of dmenu
-set -x CM_LAUNCHER "rofi"
-
-# # music player daemon
-# if test $hostname = "asterix"
-#     set -x MPD_HOST (ifconfig wlan0 | grep 'inet ' | sed 's/.*addr:\(.*\)  Bcast.*/\1/')
-# else
-    # set -x MPD_HOST $hostname
-    set -x MPD_HOST localhost
-# end
+source $HOME/.config/fish/environment.fish
 
 
 #########################
