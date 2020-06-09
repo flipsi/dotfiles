d2b69b4ba7351242a1bce9e9a5dd1d58b728d7c3
Philipp Moers <soziflip@gmail.com>
2020-06-09 11:28:36 +0200

    add fish command to determine public IP address

--- fish/commands.fish
+++ fish/commands.fish
@@ -1,5 +1,6 @@
 # this is a collection of useful commands that might get lost if not written down
 
+curl v4.ident.me # public IP address
 curl wttr.in # weather report
 date +%s # unix timestamp
 git branch -vv --merged | grep "\[.*/.*: gone\]" | cut -d" " -f3 | xargs git branch -d # delete merged branches
