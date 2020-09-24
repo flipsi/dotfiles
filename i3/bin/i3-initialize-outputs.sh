e23383d62c7053045972e5e857457c57845e1ea9
Philipp Moers <soziflip@gmail.com>
2020-09-24 02:15:25 +0200

    i3: initialize outputs on dwarf at home

--- i3/bin/i3-initialize-outputs.sh
+++ i3/bin/i3-initialize-outputs.sh
@@ -59,12 +59,14 @@ case $(hostname) in
 
         elif xrandr | grep 'DP3-2 connected' >/dev/null; then
 
-            xrandr --output 'eDP1' --primary --mode 1920x1080 --pos 3840x0 --rotate normal --output 'DP1' --off --output 'DP2' --off --output 'DP3' --off --output 'DP3-1' --mode 1920x1080 --pos 1920x0 --rotate normal --output 'DP3-2' --mode 1920x1080 --pos 0x0 --rotate normal --output 'DP3-3' --off --output 'HDMI1' --off --output 'HDMI2' --off --output 'HDMI3' --off --output VIRTUAL1 --off \
+            xrandr --output 'eDP1' --primary \
+                --output 'DP3-2' --auto --right-of 'eDP1' \
+                --output 'DP3-1' --auto --right-of 'DP3-2'\
                 || ( \
-                sleep 0.1 && xrandr --output 'DP3-2' --off && \
-                sleep 0.1 && xrandr --output 'DP3-1' --auto && \
-                sleep 0.1 && xrandr --output 'DP3-2' --auto --right-of 'DP3-1' && \
-                sleep 0.1 && xrandr --output 'eDP1' --right-of 'DP3-2' --auto \
+                sleep 0.1 && xrandr --output 'DP3-1' --off && \
+                sleep 0.1 && xrandr --output 'DP3-2' --auto && \
+                sleep 0.1 && xrandr --output 'DP3-1' --auto --right-of 'DP3-2' && \
+                sleep 0.1 && xrandr --output 'eDP1' --auto --left-of 'DP3-2' \
                 )
 
             setup_wallpaper
