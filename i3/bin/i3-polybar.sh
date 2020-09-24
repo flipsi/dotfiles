d2a2c35c9b5339e63de7365db3a4cd540850a90a
Philipp Moers <soziflip@gmail.com>
2020-09-24 02:14:49 +0200

    polybar: multi monitor setup

--- i3/bin/i3-polybar.sh
+++ i3/bin/i3-polybar.sh
@@ -5,8 +5,8 @@ function set_env_vars() {
     export  ETH_INTERFACE
     export WLAN_INTERFACE
     export BAR_MAIN_MONITOR
-    export BAR_MAIN_DPI
-    export BAR_MAIN_HEIGHT
+    export BAR_DPI
+    export BAR_HEIGHT
     export BAR_TRAY_MAXSIZE
 
     ETH_INTERFACE=$( ip link show | grep enp | sed 's/.*: \(.*\):.*/\1/')
@@ -17,26 +17,29 @@ function set_env_vars() {
         falbala )
             if xrandr | grep -q 'DP2-1 connected 4096x2160'; then
                 BAR_MAIN_MONITOR=HDMI2
-                BAR_MAIN_DPI=300
-                BAR_MAIN_HEIGHT=60
+                BAR_DPI=300
+                BAR_HEIGHT=60
                 BAR_TRAY_MAXSIZE=28
             elif xrandr | grep -q 'DP2-1 connected'; then
                 BAR_MAIN_MONITOR=DP2-1
-                BAR_MAIN_DPI=108
-                BAR_MAIN_HEIGHT=24
+                BAR_DPI=108
+                BAR_HEIGHT=24
                 BAR_TRAY_MAXSIZE=16
             fi
             ;;
         dwarf )
-            if xrandr | grep -q 'DP3-1 connected'; then
+            if xrandr | grep -q 'DP3-2 connected'; then
                 BAR_MAIN_MONITOR='DP3-1'
-            elif xrandr | grep -q 'DP3-2 connected'; then
-                BAR_MAIN_MONITOR='DP3-2'
+                BAR_SECOND_MONITOR='DP3-2'
+                BAR_THIRD_MONITOR='eDP1'
+            elif xrandr | grep -q 'DP3-1 connected'; then
+                BAR_MAIN_MONITOR='DP3-1'
+                BAR_SECOND_MONITOR='eDP1'
             elif xrandr | grep -q 'HDMI1 connected'; then
                 BAR_MAIN_MONITOR='HDMI1'
             fi
-            BAR_MAIN_DPI=108
-            BAR_MAIN_HEIGHT=24
+            BAR_DPI=108
+            BAR_HEIGHT=24
             BAR_TRAY_MAXSIZE=16
             ;;
         * )
@@ -55,6 +58,12 @@ function start() {
     while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
     set_env_vars
     polybar main &
+    if [[ -n $BAR_SECOND_MONITOR ]]; then
+        polybar second &
+        if [[ -n $BAR_THIRD_MONITOR ]]; then
+            polybar third &
+        fi
+    fi
 }
 
 function toggle() {
