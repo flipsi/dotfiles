272e9fdb947a4cb0312383e9bb7f3a6f8cfaa402
Philipp Moers <soziflip@gmail.com>
2020-10-17 14:00:33 +0200

    i3: start new workspace numbers with 1 (not 0)

--- i3/bin/i3-new-workspace.sh
+++ i3/bin/i3-new-workspace.sh
@@ -5,7 +5,12 @@ set -e
 JSON=$(i3-msg -t get_workspaces)
 
 MAX_WORKSPACE=$(echo "$JSON" | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1)
-NEW_WORKSPACE=$((MAX_WORKSPACE + 1))
+
+if [[ "$MAX_WORKSPACE" -eq -1 ]]; then
+    NEW_WORKSPACE=1 # start with 1 instead of 0
+else
+    NEW_WORKSPACE=$((MAX_WORKSPACE + 1))
+fi
 
 
 function create_new_workspace() {
