3820cff760693f12e083115ff203a9517c99a121
Philipp Moers <soziflip@gmail.com>
2021-01-16 19:54:18 +0100

    Update goobook script

--- vim/bin/goobook-query-mail.sh
+++ vim/bin/goobook-query-mail.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
+# goobook allows to access Google Contacts via the command line.
+# This script wraps goobook to search interactively for a contacts and print their name and email.
+
 # https://atechnologyjobisnoexcuse.com/2010/04/google-contacts-in-mutt-and-vim/
 
-goobook query "$*" \
+# On Arch Linux, I recommend installing it via pip:
+#   `pip install --user goobook`
+# because for the goobook AUR package there as package conflict:
+#   Installing python-xdg will remove: python-pyxdg, python-pyxdg (python-xdg)
+
+set -e
+
+SEARCH="$*"
+
+goobook query "$SEARCH" \
     | sed -e '/^$/d' -e 's/\(.*\) \(.*\)\t.*/\1 \2/' -e 's/\(^\<.*\)\t\(.*\)/"\2" \<\1\>/' \
-    | dmenu -l 5 -fn 'Monospace-9' -i -nb \#1E1E1E -sb \#00AFAF -sf \#1E1E1E
+    | rofi -dmenu
 
