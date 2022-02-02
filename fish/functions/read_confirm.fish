a2d65c8e1e6d7f67bb4a5b17e4fd98bd10c1c176
Philipp Moers <soziflip@gmail.com>
2022-02-02 13:05:12 +0100

    fish: improve read_confirm function

--- fish/functions/read_confirm.fish
+++ fish/functions/read_confirm.fish
@@ -1,11 +1,14 @@
-function read_confirm
-	while true
-read -l -p read_confirm_prompt confirm
-switch $confirm
-case Y y j J
-return 0
-case '' N n
-return 1
-end
-end
+function read_confirm --description 'ask user for confirmation' --argument-names 'PROMPT'
+	if test -z "$PROMPT"
+		set -x PROMPT 'Continue? [y/n] '
+	end
+    while true
+        read -l -P $PROMPT confirm
+        switch $confirm
+            case Y y j J
+                return 0
+            case '' N n
+                return 1
+        end
+    end
 end
