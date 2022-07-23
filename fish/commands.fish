9fffe29e08fb529230a16ada4abdc9e0b6848402
Philipp Moers <soziflip@gmail.com>
2022-07-23 21:54:40 +0200

    fish: Add picom transparency command

--- fish/commands.fish
+++ fish/commands.fish
@@ -7,5 +7,6 @@ ssh clidle.ddns.net -p 3000 # wordle game
 date +%s # unix timestamp
 git branch -vv --merged | grep "\[.*/.*: gone\]" | cut -d" " -f3 | xargs git branch -d # delete merged branches
 git checkout --quiet --detach; git fetch (git remote | head -n1) master:master; git checkout --quiet -
+picom-trans -s -o 90 # set transparency for window to be clicked on
 yay -Sc # clear pacman cache (to free disk space)
 yay -Yc # remove unused dependencies
