f7e1bd3a0d4e11750e468717a020f77cbb55f687
Philipp Moers <soziflip@gmail.com>
2022-07-27 20:55:58 +0200

    Add norman layout

--- install.sh
+++ install.sh
@@ -501,6 +501,7 @@ function create_link_for_target() {
             if [[ $(whoami) = 'root' ]]; then
                 create_link "$PWD/xkb/symbols/de_sflip" "/usr/share/X11/xkb/symbols/de_sflip"
                 create_link "$PWD/xkb/symbols/us_sflip" "/usr/share/X11/xkb/symbols/us_sflip"
+                create_link "$PWD/xkb/symbols/us_norman_sflip" "/usr/share/X11/xkb/symbols/us_norman_sflip"
             fi
             setxkbmap us_sflip
             mkdir -p "$HOME/.config/autostart"
