7b01bd225aba49157fef4350157f4a279cd78851
Philipp Moers <soziflip@gmail.com>
2023-03-02 22:55:14 +0100

    tmux: improve session setup

--- tmux/tmux-initialize-sessions.sh
+++ tmux/tmux-initialize-sessions.sh
@@ -105,9 +105,8 @@ function create_sessions() {
 
 
 create_sessions
+tmux switch-client -t $SESSION_NAME_1
 
 if (tmux has-session -t init 2>/dev/null); then
     tmux kill-session -t init
 fi
-
-tmux switch-client -t $SESSION_NAME_1
