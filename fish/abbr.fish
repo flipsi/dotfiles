f8d9bf02d2a39dca615c4ed853f48fe3e18142cd
Philipp Moers <soziflip@gmail.com>
2023-07-13 23:08:48 +0200

    fish: add git cherry-pick abbr

--- fish/abbr.fish
+++ fish/abbr.fish
@@ -53,6 +53,7 @@ abbr $args ge     git checkout
 abbr $args ge-    git checkout -
 abbr $args gem    git checkout \(git-main-or-master\)
 abbr $args geb    git checkout -b
+abbr $args gcp    git cherry-pick
 abbr $args gf     git fetch --prune
 abbr $args gfm    git checkout --quiet --detach\; git fetch \(git remote \| head -n1\) \(git-main-or-master\):\(git-main-or-master\)\; git checkout --quiet -
 abbr $args ghpr   gh pr view --web \|\| gh pr create --web
