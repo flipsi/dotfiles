25509d992e0b8e61e43d47684e6d3bd064815aa8
Philipp Moers <soziflip@gmail.com>
2023-09-22 12:01:27 +0200

    i3: fix/improve mute call window matching

--- i3/bin/mute-video-call.sh
+++ i3/bin/mute-video-call.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
+set -e
+
+# Look for WM_NAME in `xprop` output, e.g.
+# WM_NAME(UTF8_STRING) = "Google Meet - Meet - qup-bwhq-mbc"
+
+WINDOW_NAME_GOOGLE_MEET='Meet - .+'
+WINDOW_NAME_SLACK=' - Slack'
 
 function mute_or_unmute_google_meet() {
-    xdotool search --name '^Meet - .+ - Chromium$' \
+    xdotool search --name "$WINDOW_NAME_GOOGLE_MEET" \
         windowactivate --sync \
         sleep 0.1 \
         key ctrl+d
 }
 
 function mute_or_unmute_slack_huddle() {
-    xdotool search --name '^Slack - .+ - Huddle$' \
+    xdotool search --name "$WINDOW_NAME_SLACK" \
         windowactivate --sync \
         sleep 0.1 \
         key ctrl+shift+space
@@ -17,4 +24,4 @@ function mute_or_unmute_slack_huddle() {
 
 ACTIVE_WINDOW=$(xdotool getactivewindow)
 mute_or_unmute_slack_huddle || mute_or_unmute_google_meet
-xdotool windowactivate $ACTIVE_WINDOW
+xdotool windowactivate "$ACTIVE_WINDOW"
