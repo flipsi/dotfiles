db67d032763735bbee5ace71341959cb0fae81e3
Philipp Moers <soziflip@gmail.com>
2023-10-11 10:13:09 +0200

    fish: add ss TCP check to saved commands

--- fish/commands.fish
+++ fish/commands.fish
@@ -8,6 +8,7 @@ docker run -it --rm alpine
 git branch -vv --merged | grep "\[.*/.*: gone\]" | cut -d" " -f3 | xargs git branch -d # delete merged branches
 git checkout --quiet --detach; git fetch (git remote | head -n1) master:master; git checkout --quiet -
 picom-trans -s -o 90 # set transparency for window to be clicked on
+ss -lt src :5432 # show all TCP sockets listening on a specific port
 ssh clidle.ddns.net -p 3000 # wordle game
 sudoedit /etc/X11/xorg.conf.d/00-keyboard.conf # configure Xorg keyboard layout
 yay -Sc # clear pacman cache (to free disk space)
