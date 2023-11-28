f4b914dde3ebe347c59fa1aeb7ef9ce5a4262aca
Philipp Moers <soziflip@gmail.com>
2023-11-28 13:46:13 +0100

    i3: fix outputs with HDMI2 on falbala

--- i3/bin/i3-initialize-outputs.sh
+++ i3/bin/i3-initialize-outputs.sh
@@ -184,17 +184,21 @@ function main() {
 
                 disable_all_but_one_output
 
+            elif xrandr | grep -q 'HDMI2 connected'; then
+
+                MAIN_MONITOR='DP1'
+                SECOND_MONITOR='HDMI2'
+                THIRD_MONITOR='DisplayPort-0'
+                LAPTOP_SCREEN='eDP1'
+                arrange_outputs_at_home
+
             elif xrandr | grep -q 'DVI-I-1-1 connected'; then
 
                 MAIN_MONITOR='DP1'
                 SECOND_MONITOR='DVI-I-1-1'
                 THIRD_MONITOR='DVI-I-2-2'
                 LAPTOP_SCREEN='eDP1'
-
-                xrandr --output "$LAPTOP_SCREEN" --auto \
-                    --output "$SECOND_MONITOR" --auto --above "$LAPTOP_SCREEN" \
-                    --output "$MAIN_MONITOR" --primary --auto --left-of "$SECOND_MONITOR" \
-                    --output "$THIRD_MONITOR" --off
+                arrange_outputs_at_home
 
             elif xrandr | grep -q 'DP1 connected'; then
 
