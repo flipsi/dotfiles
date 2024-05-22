8a4cae6ad1de95308b20a916869b863cd7ea0e4c
Philipp Moers <soziflip@gmail.com>
2024-05-22 08:21:04 +0200

    fish: add go bin dir to PATH

--- fish/environment.fish
+++ fish/environment.fish
@@ -11,15 +11,16 @@ function prepend_to_path
 end
 
 prepend_to_path ~/.babashka/bbin/bin
-prepend_to_path ~/.local/share/coursier/bin
-prepend_to_path ~/.local/bin
 prepend_to_path ~/.cargo/bin
-prepend_to_path ~/cifuzz/bin
 prepend_to_path ~/.gem/ruby/2.4.0/bin
 prepend_to_path ~/.gem/ruby/2.6.0/bin
 prepend_to_path ~/.i3/bin
-prepend_to_path ~/shellscripts
+prepend_to_path ~/.local/bin
+prepend_to_path ~/.local/share/coursier/bin
 prepend_to_path ~/bin
+prepend_to_path ~/cifuzz/bin
+prepend_to_path ~/go/bin
+prepend_to_path ~/shellscripts
 
 
 # edit files with neovim or vim
