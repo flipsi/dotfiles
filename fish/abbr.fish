2a981bf421d8fab3fab26520876dd9aef402e21f
Philipp Moers <soziflip@gmail.com>
2024-07-09 02:52:18 +0200

    fish: add abbr for `echo $status`

--- fish/abbr.fish
+++ fish/abbr.fish
@@ -26,6 +26,7 @@ abbr $args dco    docker-compose stop
 abbr $args dcl    docker-compose logs
 abbr $args dclf   docker-compose logs -f
 abbr $args dcp    docker-compose pull
+abbr $args es     echo \$status
 abbr $args fi     fzf_wrapper --find --ranger
 abbr $args fl     fzf_wrapper --locate --ranger
 abbr $args g      git
