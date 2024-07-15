5a3e89dfffce2b1b9ce95ecae7f682a796b7d32f
Philipp Moers <soziflip@gmail.com>
2024-07-15 19:50:28 +0200

    fish: make bak function silent

--- fish/functions/bak.fish
+++ fish/functions/bak.fish
@@ -2,7 +2,7 @@ function bak --description 'make a copy of files as a backup'
     set timestamp (date +%Y-%m-%d-%H-%M)
     for file in $argv
         set backup_file "$file.$timestamp.bak"
-        if touch "$backup_file"
+        if touch "$backup_file" > /dev/null 2> /dev/null
             cp -r "$file" "$backup_file"
         else
            sudo cp -r "$file" "$backup_file"
