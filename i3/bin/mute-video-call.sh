a245d875468609abeb36b357ab357d5480fb2b6f
Philipp Moers <soziflip@gmail.com>
2024-10-23 23:11:24 +0200

    i3: improve mute microphone script

--- i3/bin/mute-video-call.sh
+++ i3/bin/mute-video-call.sh
@@ -2,25 +2,34 @@
 
 set -e
 
+# This mutes and unmutes the microphone for video calls and is meant to be bound to a shortcut.
+
 # Look for WM_NAME in `xprop` output, e.g.
 # WM_NAME(UTF8_STRING) = "Google Meet - Meet - qup-bwhq-mbc"
+# to configue WINDOW_* variables
 
-WINDOW_NAME_GOOGLE_MEET='Meet - .+'
-WINDOW_NAME_SLACK=' - Slack'
+SLEEP_INTERVAL=0.1
 
 function mute_or_unmute_google_meet() {
+    local WINDOW_NAME_GOOGLE_MEET='Meet - .+'
     xdotool search --name "$WINDOW_NAME_GOOGLE_MEET" \
         windowactivate --sync \
-        sleep 0.1 \
+        sleep "$SLEEP_INTERVAL" \
         key ctrl+d
 }
 
 function mute_or_unmute_slack_huddle() {
-    xdotool search --name "$WINDOW_NAME_SLACK" \
+    local WINDOW_NAME_SLACK_MAIN=' - Slack'
+    local WINDOW_NAME_SLACK_HUDDLE='^Slack - .+ - Huddle$'
+    xdotool search --name "$WINDOW_NAME_SLACK_HUDDLE" \
         windowactivate --sync \
-        sleep 0.1 \
+        sleep "$SLEEP_INTERVAL" \
+        key ctrl+shift+space \
+    || xdotool search --name "$WINDOW_NAME_SLACK_MAIN" \
+        windowactivate --sync \
+        sleep "$SLEEP_INTERVAL" \
         key ctrl+shift+space
-}
+    }
 
 ACTIVE_WINDOW=$(xdotool getactivewindow)
 mute_or_unmute_google_meet || mute_or_unmute_slack_huddle
