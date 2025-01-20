857dc9acbbdbe405f250e8999557dc38f77bf880
Philipp Moers <soziflip@gmail.com>
2025-01-20 11:35:31 +0100

    i3: fix muting MS Teams

--- i3/bin/mute-video-call.sh
+++ i3/bin/mute-video-call.sh
@@ -12,15 +12,15 @@ SLEEP_INTERVAL=0.1
 
 function mute_or_unmute_google_meet() {
     local WINDOW_NAME_GOOGLE_MEET='Meet - .+'
-    xdotool search --name "$WINDOW_NAME_GOOGLE_MEET" \
+    xdotool search --onlyvisible --name "$WINDOW_NAME_GOOGLE_MEET" \
         windowactivate --sync \
         sleep "$SLEEP_INTERVAL" \
         key ctrl+d
 }
 
 function mute_or_unmute_microsoft_teams() {
-    local WINDOW_NAME_MS_TEAMS='.* \| Microsoft Teams.*'
-    xdotool search --name "$WINDOW_NAME_MS_TEAMS" \
+    local WINDOW_NAME_MS_TEAMS='Microsoft Teams.*'
+    xdotool search --onlyvisible --name "$WINDOW_NAME_MS_TEAMS" \
         windowactivate --sync \
         sleep "$SLEEP_INTERVAL" \
         key ctrl+shift+m
@@ -29,19 +29,23 @@ function mute_or_unmute_microsoft_teams() {
 function mute_or_unmute_slack_huddle() {
     local WINDOW_NAME_SLACK_MAIN=' - Slack'
     local WINDOW_NAME_SLACK_HUDDLE='^Slack - .+ - Huddle$'
-    xdotool search --name "$WINDOW_NAME_SLACK_HUDDLE" \
+    xdotool search --onlyvisible --name "$WINDOW_NAME_SLACK_HUDDLE" \
         windowactivate --sync \
         sleep "$SLEEP_INTERVAL" \
         key ctrl+shift+space \
-    || xdotool search --name "$WINDOW_NAME_SLACK_MAIN" \
+    || xdotool search --onlyvisible --name "$WINDOW_NAME_SLACK_MAIN" \
         windowactivate --sync \
         sleep "$SLEEP_INTERVAL" \
         key ctrl+shift+space
-    }
+}
 
-ACTIVE_WINDOW=$(xdotool getactivewindow)
-false \
-    || mute_or_unmute_microsoft_teams \
-    || mute_or_unmute_google_meet \
-    || mute_or_unmute_slack_huddle
-xdotool windowactivate "$ACTIVE_WINDOW"
+function main() {
+    ACTIVE_WINDOW=$(xdotool getactivewindow)
+    false \
+        || mute_or_unmute_microsoft_teams \
+        || mute_or_unmute_google_meet \
+        || mute_or_unmute_slack_huddle
+            xdotool windowactivate "$ACTIVE_WINDOW"
+}
+
+main
