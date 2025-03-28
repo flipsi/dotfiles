efa69755db6b50435542a8f7f8f8eebfb3b5f838
Philipp Moers <soziflip@gmail.com>
2025-03-28 23:20:34 +0100

    fish: updating alt bindings

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -4,10 +4,10 @@ function fish_user_key_bindings
     bind \e, history-token-search-backward
     bind \e. history-token-search-forward
     bind \ec 'history | head -n 1 | tr -d "\n" | xsel -b'
-    # bind \ei 'joshuto ; or ranger ; and clear; and fish_prompt' # opens ranger also on errors
-    bind \ei 'joshuto ; and clear; and fish_prompt' # opens ranger also on errors
-    bind \ej 'cd_interactively; and clear; and fish_prompt'
+    # bind \ei 'joshuto ; and clear; and fish_prompt'
+    bind \ei 'joshuto ; or ranger ; and clear; and fish_prompt' # opens ranger also on errors
     bind \el 'commandline ""; echo; exa_or_ls -l; fish_prompt'
+    bind \eo 'cd_interactively; and clear; and fish_prompt'
     bind \et 'commandline -i (echo -n (date +"%F-%H%M%S"))'
     bind \eu 'commandline ""; cd ..; clear; fish_prompt'
     bind \ew 'watch -c (history | head -n 1)'
