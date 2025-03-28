16ac8973c95563de5ea0f4fd0fefc948521f4015
Philipp Moers <soziflip@gmail.com>
2025-03-28 23:41:53 +0100

    cd_interactively in joshuto/ranger

--- fish/functions/cd_interactively.fish
+++ fish/functions/cd_interactively.fish
@@ -1,9 +1,12 @@
-function cd_interactively --description 'cd into a directoy interactively' --argument-names 'ROOT'
+function cd_interactively --description 'cd into a directoy interactively' --argument-names 'ROOT' --argument-names 'COMMAND'
     if test ! -d "$ROOT"
         set ROOT $HOME
     end
     set -l DIR (fd --type d . $ROOT | fzf)
     if test "$DIR" != ""
         cd $DIR
+        if command -v "$COMMAND" >/dev/null
+            "$COMMAND"
+        end
     end
 end
